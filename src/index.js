7722ea46528f241ab702b62f7ba1cc7f6e4a1faa
YentlBekaert <yentl.bekaert@student.vives.be>
2021-09-30 16:44:18 +0200

    setting_up_webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import _ from 'lodash';
-import './style.css';
+import './styles/main.scss';
 
 function component() {
     const element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,43 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
+  //mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  }, 
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader', 
+          'css-loader',
+          'sass-loader'
+        ],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+  }),
+  ]
 };
